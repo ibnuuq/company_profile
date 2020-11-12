1c7b695079758c4e557e40d564f45bbf8dde095a
ibnuuq <ibnuuqq@gmail.com>
2020-11-12 14:23:12 +0630

    adding bootstrap

---
+++ cssnative.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>PT Internetslim</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/stylenative.css">
+</head>
+
+<body>
+	<table border="0" width="100%" height="100%">
+		<tr>
+			<td class="padding-10">
+				<h3><span>InternetSlim</span> &nbsp;&nbsp;&nbsp;</h3>
+			</td>
+			<td>Home</td>
+			<td>About</td>
+			<td>Produk</td>
+			<td>Kontak</td>
+		</tr>
+		<tr>
+			<td colspan="5">
+				<img src="assets/images/Capture.jpg" width="100%" height="100%">
+			</td>
+		</tr>
+
+		<tr>
+			<td colspan="5">
+
+				<center>
+					<h2>PRODUK BARU</h2>
+					<p>Kami memiliki produk baru yang siap mempermudah Anda dalam proses antrian. Di masa pandemi Covid-19 ini aplikasi antrian sangat dibutuhkan untuk mengurangi penumpukan antrian di suatu instansi. Aplikasi ini dijalankan secara online sehingga para pengguna aplikasi dapat mendaftarkan diri dalam proses antrian di suatu instansi di mana saja dan kapan saja.
+					</p>
+					<button class="btn">Demo Aplikasi</button>
+
+				</center>
+				<br>
+				<hr>
+			</td>
+		</tr>
+		<tr>
+			<td style="padding-left: 20px;">
+				<br>
+				<br>
+				<h3>Tentang Kami</h3>
+				<p>Kami PT Internet Solusi Layanan Informasi Mandiri (InternetSlim) <br>
+					merupakan sebuah perusahaan multi-nasional yang sudah memiliki <br>
+					berbagai pengalaman dalam berbagai proyek-proyek sektor informasi <br>
+					digital baik di instansi pemerintahan, BUMN, perusahaan swasta, maupun<br>lembaga baik dalam maupun luar negeri.</p>
+
+			</td>
+			<td colspan="2" bgcolor="#4ac4f3" class="padding-10 box-biru">
+				<img src="assets/images/webgis.png" width="50px" height="50px"><br>
+				<h3>WebGis</h3>
+				<p>Menampilkan dan mengolah data <br>
+					Geographic Information System (GIS) <br>
+					secara realtime.<br></p>
+			</td>
+			<td colspan="2" bgcolor="#4ac4f3" class="padding-10 box-putih">
+				<img src="assets/images/monitoringgis.png" width="50px" height="50px"><br>
+				<h3>Monitoring Gis</h3>
+				<p>Monitoring perluasan,<br>
+					penyempitan, perpindahan <br>
+					objek secara realtime.<br></p>
+			</td>
+		</tr>
+		<tr>
+			<td style="padding-left: 20px;">
+				<hr>
+				<h3>JASA PEMBUATAN WEBGIS</h3>
+				<p>Webgis merupakan sistem yang dirancang untuk bekerja <br>
+					dengan data yang tereferensi secara spasial atau <br>
+					koordinat-koordinat geografi.<br></p>
+				<ul>
+					<li>WebGIS</li>
+					<li>Monitoring GIS</li>
+					<li>Tracking System</li>
+					<li>Custom GIS</li>
+				</ul>
+			</td>
+			<td colspan="2" bgcolor="#4ac4f3" class="padding-10 box-putih">
+				<br>
+				<img src="assets/images/tracking.png" width="50px" height="50px"><br>
+				<h3>Tracking System</h3>
+				<p>Monitoring jalur pergerakan objek <br>
+					secara realtime dengan menggunakan <br>
+					website dan smartphone. <br></p>
+			</td>
+			<td colspan="2" bgcolor="#4ac4f3" class="padding-10 box-biru">
+				<br>
+				<img src="assets/images/edit.png" width="50px" height="50px"><br>
+				<h3>Custom Gis</h3>
+				<p>Kami dapat membuat web dan aplikasi <br>
+					GIS apapun sesuai dengan apa yang Anda <br>
+					perlukan.<br></p>
+
+			</td>
+		</tr>
+		<tr>
+			<td colspan="5">
+				<br>
+				<center>
+					<h2>JASA PEMBUATAN CUSTOM WEB DAN MOBILE</h2>
+					<p>Bukan hanya sekedar pembuatan website, tapi Kami juga melayani berbagai pembuatan aplikasi lain yang difokuskan<br>
+						untuk menyesuaikan kebutuhan dari perusahaan Anda.</p>
+				</center>
+				<hr>
+			</td>
+		</tr>
+		<tr>
+			<td rowspan="2">
+				<img src="assets/images/app-1.png">
+			</td>
+			<td colspan="2" bgcolor="#4ac4f3" class="padding-10 box-biru">
+				<img src="assets/images/company.png" width="50px" height="50px"><br>
+				<h3>Tracking System</h3>
+				<p>Monitoring jalur pergerakan objek <br>
+					secara realtime dengan menggunakan <br>
+					website dan smartphone. <br></p>
+			</td>
+			<td colspan="2" bgcolor="#4ac4f3" class="padding-10 box-putih">
+				<img src="assets/images/government-buildings.png" width="50px" height="50px"><br>
+				<h3>Custom Gis</h3>
+				<p>Kami dapat membuat web dan aplikasi <br>
+					GIS apapun sesuai dengan apa yang Anda <br>
+					perlukan.<br></p>
+
+			</td>
+		</tr>
+		<tr>
+			<td colspan="2" bgcolor="#4ac4f3" class="padding-10 box-putih">
+				<img src="assets/images/shopping-cart.png" width="50px" height="50px"><br>
+				<h3>Tracking System</h3>
+				<p>Monitoring jalur pergerakan objek <br>
+					secara realtime dengan menggunakan <br>
+					website dan smartphone. <br></p>
+			</td>
+			<td colspan="2" bgcolor="#4ac4f3" class="padding-10 box-biru">
+				<img src="assets/images/pay-day.png" width="50px" height="50px"><br>
+				<h3>Custom Gis</h3>
+				<p>Kami dapat membuat web dan aplikasi <br>
+					GIS apapun sesuai dengan apa yang Anda <br>
+					perlukan.<br></p>
+
+			</td>
+		</tr>
+		<tr>
+			<td colspan="5">
+				<br>
+				<center>
+					<h2>KONTAK KAMI</h2>
+					<p>Jika ingin menghubungi Kami, maka berikut beberapa informasi kontak dari Kami yang dapat dihubungi. Kami siap <br> memberikan solusi dan kemudahan kepada Anda melalui aplikasi website, GIS, dan aplikasi mobile.</p>
+				</center>
+				<hr>
+			</td>
+		</tr>
+		<tr>
+			<td style="padding: 20px;">
+				<h3>ALAMAT KANTOR KAMI</h3>
+				<p>Jl. Sentosa Raya no. 14, Baktijaya, Sukmajaya,<br>
+					Depok - Jawa Barat<br>
+					16411<br>
+					Office (021) 7714336<br>
+					Saleh 0821 - 1134 - 2861<br>
+					Alfyan 0853 - 5724 - 5393<br>
+					contact@internetslim.com</p>
+			</td>
+			<td colspan="4">
+
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.9967491891193!2d106.83471961434228!3d-6.394419364320875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ec068218019b%3A0xde031f680a8796a4!2sInternetSlim.+PT!5e0!3m2!1sid!2sid!4v1554886008195!5m2!1sid!2sid" width="90%" height="100%"></iframe>
+
+			</td>
+		</tr>
+		<tr>
+			<td colspan="5" style="padding: 20px;">
+				<p>Copyright 2020 PT Internet Solusi Layanan Informasi Mandiri (InternetSlim)<br>
+
+					#ONE STOP WEB SOLUTION</p>
+			</td>
+		</tr>
+	</table>
+
+</body>
+
+</html>
